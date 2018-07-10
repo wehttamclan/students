09d7917de268315fd5ceeb920accb7a119b43875
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-10 13:52:58 -0600

    adds rspec and initial test

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Student do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        student = Student.new()
+        expect(student).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      xit "is valid with a name" do
+        student = Student.new(name: "Dropbox")
+        expect(student).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+  end
+
+  describe 'class methods' do
+  end
+end
