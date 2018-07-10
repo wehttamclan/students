4564b47ad210beba8bdd6b75461e71423d07781c
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-10 15:15:31 -0600

    adds create method in controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,18 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   def show
     @student = Student.find(params[:id])
   end
+
+  private
+    def student_params
+      params.require(:student).permit(:name)
+    end
 end
--- spec/features/students/new_page_spec.rb
+++ spec/features/students/new_page_spec.rb
@@ -6,16 +6,16 @@ describe "User visits students/new page" do
 
     click_link "Create Student"
 
-    expect(current_path).to eq("/students")
+    expect(current_path).to eq("/students/new")
 
     homer = "Homer Simpson"
-    fills_in "student[name]", with: homer
+    fill_in "student[name]", with: homer
 
-    click_button "Create"
+    click_button "Create Student"
 
-    within ".students" do
-      expect(page).to have_content(homer)
-      expect(page).to_not have_content("Bart Simpson")
-    end
+    expect(current_path).to eq(student_path(Student.last))
+
+    expect(page).to have_content(homer)
+    expect(page).to_not have_content("Bart Simpson")
   end
 end
