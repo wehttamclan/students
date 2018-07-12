cbdf6fc38176095dfb3b2fd6b419338a58842470
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-11 22:38:59 -0600

    validates relationships

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 describe Address do
-  it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:street) }
-  it { should validate_presence_of(:city) }
-  it { should validate_presence_of(:state) }
-  it { should validate_presence_of(:zip) }
+  context 'validations' do
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:street) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip) }
+  end
+  context 'relationships' do
+    it { should belong_to :student }
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 describe Student do
   describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a name" do
-        student = Student.new()
-        expect(student).to be_invalid
-      end
-    end
-
     context "valid attributes" do
       it { should validate_presence_of(:name) }
     end
