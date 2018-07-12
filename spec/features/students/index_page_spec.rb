36fedc8dc5a721243d851639378609fac9f17bbb
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-11 22:05:53 -0600

    fixed delete button on students index page

--- spec/features/students/index_page_spec.rb
+++ spec/features/students/index_page_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe "User sees all students" do
   before :each do
-    @student_1 = Student.create(name: "Bart Simpson")
-    @student_2 = Student.create(name: "Lisa Simpson")
+    @student_1 = Student.create(name: "Bart Simpson", id: 1)
+    @student_2 = Student.create(name: "Lisa Simpson", id: 2)
   end
   scenario "a user sees a student" do
     visit students_path
 
-    expect(current_path).to eq("/students")
+    expect(current_path).to eq(students_path)
     within ".students" do
       expect(page).to have_content("#{@student_1.name}")
       expect(page).to have_content("#{@student_2.name}")
@@ -17,11 +17,11 @@ describe "User sees all students" do
   scenario "a teacher destroys a student" do
     visit students_path
 
-    expect(current_path).to eq("/students")
+    expect(current_path).to eq(students_path)
 
-
-    click_link "Delete #{@student_1.name}"
-
+    within "##{@student_1.id}" do
+      click_link "Delete"
+    end
 
     expect(page).to_not have_content("#{@student_1.name}")
     expect(page).to have_content("#{@student_2.name}")
